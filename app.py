b0f459cb963582e7a7914e7a7be6aaf9d64928c3
Sheng Je <35686590+shengje@users.noreply.github.com>
2019-10-10 16:53:48 +0800

    first commit
    first commit to test the heroku

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('0c360ea2ab1a5b3dcce105d393d74de9')
+handler = WebhookHandler('X2ga8blI+bQgTq52xUBEpvTQbOzJXIr8nLA05yXX6ok4hHB4vDe8Y85ZQBkvfPKOv+WkfaUP7aq5EJbiwAqO9J6+rK850T67BhF/AQCQ+UBvfaKqxTW9j9FtX3QZXX5qyWNV239YskeO/CE7+0YPaQdB04t89/1O/w1cDnyilFU=')
+
+
+@app.route("/")
+def home():
+    return 'home OK'
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
+    
